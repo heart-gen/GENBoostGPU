6c69c979e6a51ed525ced7ae02a16ebf0dd8d765
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-09-13 20:48:57 -0400

    update to add helper functions for cpg case

--- src/genboostgpu/__init__.py
+++ src/genboostgpu/__init__.py
@@ -1,9 +1,15 @@
 from . import data_io
+from . import pipeline
 from . import enet_boosting
 from . import snp_processing
 
 from .enet_boosting import boosting_elastic_net
 from .data_io import load_genotypes, load_phenotypes, save_results
+from .pipeline import (
+    prepare_cpg_inputs,
+    run_boosting_for_cpgs,
+    run_boosting_for_cpg_delayed
+)
 from .snp_processing import (
     preprocess_genotypes,
     filter_zero_variance,
@@ -22,7 +28,11 @@ __all__ = [
     "load_genotypes",
     "load_phenotypes",
     "save_results",
+    "prepare_cpg_inputs",
+    "run_boosting_for_cpgs",
+    "run_boosting_for_cpg_delayed",
     "data_io",
+    "pipeline",
     "enet_boosting",
     "snp_processing",
 ]
